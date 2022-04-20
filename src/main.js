5566c27ff0350b1b2deadd3d7aed59624c9991b7
suzhongde <2373544536@qq.com>
2022-04-20 08:54:03 +0800

    feat: 用户鉴权

--- src/main.js
+++ src/main.js
@@ -1,20 +1,26 @@
-import { createApp } from 'vue'
-import { Quasar } from 'quasar'
+import { createApp } from 'vue';
+import { Quasar } from 'quasar';
 import router from './router';
+import store from './store';
+import './permission'; // permission control
 
 // Import icon libraries
-import '@quasar/extras/material-icons/material-icons.css'
+import '@quasar/extras/material-icons/material-icons.css';
 
 // Import Quasar css
-import 'quasar/src/css/index.sass'
+import 'quasar/src/css/index.sass';
 
-import App from './App.vue'
+import App from './App.vue';
 
-const myApp = createApp(App)
+const myApp = createApp(App);
 
 myApp.use(router);
+myApp.use(store);
+if (process.env.NODE_ENV === 'development') {
+  myApp.config.devtools = true;
+}
 
 myApp.use(Quasar, {
-    plugins: {}, // import Quasar plugins and add here
-})
-myApp.mount('#app')
+  plugins: {} // import Quasar plugins and add here
+});
+myApp.mount('#app');
